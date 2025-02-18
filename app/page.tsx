99dcdfee83e05577e0a14bb2e4879c86e7f8567b
runhannah <leexhannah@gmail.com>
2025-02-18 08:36:59 -0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useTheme, ThemeProvider } from "../context/themeContext";
-import Navigation from "../components/navigation";
+import { useTheme, ThemeProvider } from "../context/ThemeContext";
+import Navigation from "../components/Navigation";
 import { Switch } from "../components/ui/switch";
-import ThemeIcon from "../components/themeIcon";
+import ThemeIcon from "../components/ThemeIcon";
 
 function HomeContent() {
   const { theme, setTheme } = useTheme();
@@ -16,11 +16,11 @@ function HomeContent() {
     <>
       <header>
         <div className="pt-10 flex flex-col items-start">
-          <div className="max-w-md text-primary">
-            <h1 className="font-sans text-2xl md:text-3xl pb-2">
+          <div className="max-w-md">
+            <h1 className="font-sans text-2xl md:text-3xl pb-2 text-primary">
               Hi, I&apos;m
             </h1>
-            <h2 className="font-mono text-6xl">Hannah.</h2>
+            <h2 className="font-mono text-6xl text-primary">Hannah.</h2>
           </div>
           <h2 className="font-sans sm:text-2xl md:text-3xl pt-4 pb-4 text-secondary">
             I enjoy making things with code. 👩🏻‍💻
@@ -29,19 +29,28 @@ function HomeContent() {
       </header>
       <Navigation />
       <main className={theme}>
-        <p className="text-sm md:text-base pt-4 leading-[1.8]">
+        <p className="font-bold text-base md:text-base pt-4 leading-[1.8] text-gray-800 dark:text-white">
+          * Due to recent company-wide restructuring, I am now exploring 👀 new
+          opportunities. *
+        </p>
+        <p className="text-sm md:text-base pt-4 leading-[1.8] text-gray-800 dark:text-white">
           I&apos;m a software engineer based in Los Angeles ☀️ with expertise in
           Typescript, React, Next.js, Node, PostgreSQL, MongoDB, REST APIs and
           other fun stuff. Passionate about building robust web apps and always
           learning new technologies. Former public sector professional bringing
           a unique, problem-solving mindset to tech!
         </p>
-        <p className="text-sm md:text-base pt-4 leading-[1.8]">
-          This site is a work in progress. I plan to update it with projects, so
+        <p className="text-sm md:text-base pt-4 leading-[1.8] text-gray-800 dark:text-white">
+          This site is currently work in progress. I plan to update it with projects, so
           stay tuned!
         </p>
         <div className="flex flex-row items-center align-middle mt-6">
-          <Switch checked={theme === "dark"} onCheckedChange={handleChange} />
+          <Switch
+            aria-label="toggle light/dark theme"
+            aria-checked={theme === "dark" ? "true" : "false"}
+            checked={theme === "dark"}
+            onCheckedChange={handleChange}
+          />
           <ThemeIcon theme={theme} />
         </div>
       </main>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="text-primary font-bold">
+    <nav className="font-bold">
       <ul className="flex justify-between pt-4">
         <li>
-          <Link className="font-sans" href="/">
+          <Link className="font-sans text-primary" href="/">
             Home
           </Link>
         </li>
         <li>
-          <Link className="font-sans" href="https://github.com/RunHannah">
+          <Link className="font-sans text-primary" href="https://github.com/RunHannah">
             Github
           </Link>
         </li>
         <li>
           <Link
-            className="font-sans"
+            className="font-sans text-primary"
             href="https://www.linkedin.com/in/hannahlee1/"
           >
             LinkedIn
